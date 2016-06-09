27ce6b62f6ce3973fea0166c7b307b7c01305133
Fiodor Gorobet <casusarts@gmail.com>
2016-06-09 15:59:31 +0200

    Update index.php
    Removed styles in body tag.
    Add simple description.

--- index.php
+++ index.php
@@ -2,15 +2,15 @@
 <html>
 <head lang="en">
     <meta charset="UTF-8">
-    <title></title>
+    <title>AuthSys - Demo</title>
     <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
     <script src="//notifyjs.com/dist/notify-combined.min.js"></script>
     <script src="Assets/Scripts/md5.min.js"></script>
     <script src="Controller/app.js"></script>
 </head>
-<body style="background-color: #547421">
-<script>
-    //document.write(md5('TEST'));
-</script>
+<body>
+    <h1>AuthSys - Demo</h1>
+    <p>Use JS console for API calls.</p>
+<script></script>
 </body>
 </html>
